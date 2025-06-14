3b19d5bbc450d5197abda27b9fd6e104e5f5b216
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-06-14 06:29:14 +0530

    Intialized express backend in code .

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import { config as _config } from 'dotenv';
+import cors from 'cors';
+_config();
+
+class App {
+    private app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.initializeRoutes();
+        this.initializeMiddlewares();
+    }
+
+    private initializeRoutes() {
+        this.app.get('/health-check', (req, res) => {
+            res.status(200).json({ message: 'Backend is healthy' });
+        });
+    }
+    private initializeMiddlewares() {
+        this.app.use(cors({
+            origin: [
+                'http://localhost:3000',
+            ],
+            credentials: true
+        }));
+        this.app.use(express.json())
+    }
+
+
+    public listen(port: number) {
+        this.app.listen(port || 3001, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
+}
+
+const app = new App();
+app.listen(parseInt(process.env.PORT || '3001'))
